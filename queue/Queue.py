66a7fab6387506cb106b52f11707e7384c87ffc8
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2024-07-24 16:03:26 -0400

    add queue implementation in array and testing

--- queue/Queue.py
+++ queue/Queue.py
@@ -1,10 +1,13 @@
 from ListNode import ListNode
 #implementation of Queue: fifo dsa
-class QueueArray():
+
+class QueueArray(): #FIXED SIZE ARRAY IMPLEMENTATION
     def __init__(self, max_size):
         self.queue = [None]*max_size
         self.max_size = max_size
         self.size = 0
+        self.head = 0
+        self.tail = -1
     
     def __len__(self):
         return self.size
@@ -17,12 +20,38 @@ class QueueArray():
         
     
     def enqueue(self, value):
-        self.queue
+        if self.is_full() == True:
+            raise IndexError("The queue is full")
+        self.tail = (self.tail+1)%self.max_size
+        self.queue[self.tail] = value
+        self.tail = self.tail+1
+        
 
     def dequeue(self):
-        pass
+        if self.is_empty() == True:
+            raise IndexError("The queue is empty")
+        removed = self.queue[self.head]
+        self.queue[self.head] = None
+        self.head = (self.head+1)%self.max_size
+        self.head = self.head+1
+        return removed
 
-class Queue(): #linked list implementation
+    def __str__(self):
+        index = self.head
+        str = ""
+        if self.is_empty == True:
+            raise IndexError("Queue is empty")
+        for i in range(self.size):
+            str = str(self.queue[index]) + "-->"
+            index = (index + 1)%self.max_size
+        return str
+    
+    def peek(self):
+        if self.is_empty() == True:
+            raise IndexError("The queue is empty")
+        return self.queue[self.head]
+    
+class Queue(): #LINKED LIST IMPLEMENTATION
     def __init__(self, max_size):
         self.head = None #first element to be popped 
         self.tail = None #last element to be added
@@ -36,6 +65,7 @@ class Queue(): #linked list implementation
         node = ListNode(value)
         if self.is_full() == True:
             raise IndexError("The queue is full")
+             
         else:
             self.size += 1
 
@@ -46,7 +76,6 @@ class Queue(): #linked list implementation
             self.tail.next = node
             self.tail = node
         
-
     def dequeue(self):
         if len(self) == 0:
             raise IndexError("The queue is empty")
--- queue/main.py
+++ queue/main.py
@@ -1,31 +1,7 @@
-from Queue import Queue
+from Queue import QueueArray, Queue
 
 class TestLLQueue:
 
-    def initialize(queue: Queue):
-        queue.enque(3)
-        queue.enque(5)
-        queue.enque(6)
-        queue.enque(1)
-        print(f"Create initial queue: {queue}")
-
-    def test_pop(queue:Queue):
-        print(f"POP! The removed value {queue.deque()}") 
-        print(f"New Queue: {queue}") 
-
-    def test_peek(queue:Queue):
-        print(f"Top of the queue value: {queue.peek()}") 
-
-    def is_full(queue:Queue):
-        print(f"Check if queue is full: {queue.is_full()}") 
-
-    def test_llqueue(self,queue:Queue):
-        self.initialize(queue)
-        self.is_full(queue)
-        self.test_pop(queue)
-        self.test_peek(queue)
-
-class TestArrQueue:
     def initialize(queue: Queue):
         queue.enqueue(3)
         queue.enqueue(5)
@@ -49,10 +25,41 @@ class TestArrQueue:
         self.test_pop(queue)
         self.test_peek(queue)
 
+class TestArrQueue:
+    def initialize(queue: QueueArray):
+        queue.enqueue(3)
+        queue.enqueue(5)
+        queue.enqueue(6)
+        queue.enqueue(1)
+        queue.enqueue(7)
+        print(f"Create initial queue: {queue}")
+
+    def test_pop(queue:QueueArray):
+        print(f"POP! The removed value {queue.dequeue()}") 
+        print(f"New Queue: {queue}") 
+
+    def test_peek(queue:QueueArray):
+        print(f"Top of the queue value: {queue.peek()}") 
+
+    def is_full(queue:QueueArray):
+        print(f"Check if queue is full: {queue.is_full()}") 
+
+    def test_llqueue(self,queue:QueueArray):
+        self.initialize(queue)
+        self.is_full(queue)
+        self.test_pop(queue)
+        self.test_peek(queue)
+
 
 if __name__ == "__main__":
-    s = Queue(4)
-    TestLLQueue.initialize(s)
-    TestLLQueue.is_full(s)
-    TestLLQueue.test_pop(s)
-    TestLLQueue.test_peek(s)
+    s = Queue(5)
+    ss = Queue(5)
+    # TestLLQueue.initialize(s)
+    # TestLLQueue.is_full(s)
+    # TestLLQueue.test_pop(s)
+    # TestLLQueue.test_peek(s)
+
+    TestArrQueue.initialize(ss)
+    TestArrQueue.is_full(ss)
+    TestArrQueue.test_pop(ss)
+    TestArrQueue.test_peek(ss)
