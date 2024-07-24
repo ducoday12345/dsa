e293fc25b0abe8736ba7c0b91e670989ebcb8557
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2024-07-24 15:27:22 -0400

    create class to separate testing linked list queue and fix name typo for enqueue and dequeue

--- queue/main.py
+++ queue/main.py
@@ -1,25 +1,58 @@
 from Queue import Queue
 
-def initialize(queue: Queue):
-    queue.enque(3)
-    queue.enque(5)
-    queue.enque(6)
-    queue.enque(1)
-    print(f"Create initial queue: {queue}")
+class TestLLQueue:
 
-def test_pop(queue:Queue):
-    print(f"POP! The removed value {queue.deque()}") 
-    print(f"New Queue: {queue}") 
+    def initialize(queue: Queue):
+        queue.enque(3)
+        queue.enque(5)
+        queue.enque(6)
+        queue.enque(1)
+        print(f"Create initial queue: {queue}")
 
-def test_peek(queue:Queue):
-    print(f"Top of the queue value: {queue.peek()}") 
+    def test_pop(queue:Queue):
+        print(f"POP! The removed value {queue.deque()}") 
+        print(f"New Queue: {queue}") 
 
-def is_full(queue:Queue):
-    print(f"Check if queue is full: {queue.is_full()}") 
+    def test_peek(queue:Queue):
+        print(f"Top of the queue value: {queue.peek()}") 
+
+    def is_full(queue:Queue):
+        print(f"Check if queue is full: {queue.is_full()}") 
+
+    def test_llqueue(self,queue:Queue):
+        self.initialize(queue)
+        self.is_full(queue)
+        self.test_pop(queue)
+        self.test_peek(queue)
+
+class TestArrQueue:
+    def initialize(queue: Queue):
+        queue.enqueue(3)
+        queue.enqueue(5)
+        queue.enqueue(6)
+        queue.enqueue(1)
+        print(f"Create initial queue: {queue}")
+
+    def test_pop(queue:Queue):
+        print(f"POP! The removed value {queue.dequeue()}") 
+        print(f"New Queue: {queue}") 
+
+    def test_peek(queue:Queue):
+        print(f"Top of the queue value: {queue.peek()}") 
+
+    def is_full(queue:Queue):
+        print(f"Check if queue is full: {queue.is_full()}") 
+
+    def test_llqueue(self,queue:Queue):
+        self.initialize(queue)
+        self.is_full(queue)
+        self.test_pop(queue)
+        self.test_peek(queue)
+
 
 if __name__ == "__main__":
     s = Queue(4)
-    initialize(s)
-    is_full(s)
-    test_pop(s)
-    test_peek(s)
+    TestLLQueue.initialize(s)
+    TestLLQueue.is_full(s)
+    TestLLQueue.test_pop(s)
+    TestLLQueue.test_peek(s)
