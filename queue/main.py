a6421c46886c97992af5dbad4317b4fcbede1eab
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2024-08-03 20:21:47 -0400

    add test for queue implementation by deque (collections)

--- queue/main.py
+++ queue/main.py
@@ -1,4 +1,4 @@
-from Queue import QueueArray, Queue
+from Queue import QueueArray, Queue, DeQue
 
 class TestLLQueue:
 
@@ -49,17 +49,47 @@ class TestArrQueue:
         self.is_full(queue)
         self.test_pop(queue)
         self.test_peek(queue)
+        
+class TestDeQue:
+    def initialize(queue: DeQue):
+        queue.enqueue(3)
+        queue.enqueue(5)
+        queue.enqueue(6)
+        queue.enqueue(1)
+        print(f"Create initial queue: {queue}")
+
+    def test_pop(queue:DeQue):
+        print(f"POP! The removed value {queue.dequeue()}") 
+        print(f"New Queue: {queue}") 
+
+    def test_peek(queue:DeQue):
+        print(f"Top of the queue value: {queue.peek()}") 
+
+    def is_full(queue:DeQue):
+        print(f"Check if queue is full: {queue.is_full()}") 
+
+    def test_llqueue(self,queue:DeQue):
+        self.initialize(queue)
+        self.is_full(queue)
+        self.test_pop(queue)
+        self.test_peek(queue)
 
 
 if __name__ == "__main__":
-    s = Queue(5)
-    ss = Queue(5)
-    # TestLLQueue.initialize(s)
-    # TestLLQueue.is_full(s)
-    # TestLLQueue.test_pop(s)
-    # TestLLQueue.test_peek(s)
+    t0 = Queue(5)
+    t1 = Queue(5)
+    t2 = DeQue(5)
+    TestLLQueue.initialize(t0)
+    TestLLQueue.is_full(t0)
+    TestLLQueue.test_pop(t0)
+    TestLLQueue.test_peek(t0)
 
-    TestArrQueue.initialize(ss)
-    TestArrQueue.is_full(ss)
-    TestArrQueue.test_pop(ss)
-    TestArrQueue.test_peek(ss)
+    TestArrQueue.initialize(t1)
+    TestArrQueue.is_full(t1)
+    TestArrQueue.test_pop(t1)
+    TestArrQueue.test_peek(t1)
+    
+    TestArrQueue.initialize(t2)
+    TestArrQueue.is_full(t2)
+    TestArrQueue.test_pop(t2)
+    TestArrQueue.test_peek(t2)
