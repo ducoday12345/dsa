2f9a6a9adffa3178a1de46c8ea4021b26e2f0859
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2024-08-07 07:50:39 -0400

    implementation of Hashtable using array

--- hash_table/HashTable.py
+++ hash_table/HashTable.py
@@ -1,3 +1,44 @@
 class HashTable:
-    def __init__(self):
-        self.hash
+    def __init__(self, max=10):
+        self.table = [[] for _ in range(max)]
+        self.max = max
+        self.count = 0 #length
+        
+    def _hash(self,key):
+        return hash(key) %self.max # compute the hash value => location in the hash
+    
+    def __len__(self):
+        return self.count
+    
+    def __str__(self):
+        return self.table
+    
+    def insert(self, key, value):
+        index = self._hash(key)
+        for i in self.table[index]:
+            if i[0] == key:
+                i[1] = value # update value if key existed
+                return
+        self.table[index].append([key,value])
+        self.count+=1
+        
+    def delete(self, key):
+        index = self._hash(key)
+        for i, pair in enumerate(self.table[index]):
+            if pair[0] == key:
+                self.table[i] = []
+                self.count -= 1
+                return
+            else:
+                raise KeyError(f"{key} does not exist in hash table")
+            
+    def exists(self, key):
+        index = self._hash(key)
+        for i, pair in enumerate(self.table[index]):
+            if pair[0] == key:
+                return True
+            else:
+                return False
+            
+    
+        
