10969882fa557b68f1bf593741ed7e811fa1ee6a
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2024-08-08 15:43:03 -0400

    add test for binary search

---
+++ binary_search/main.py
@@ -0,0 +1,20 @@
+from BinarySearch import BinarySearch
+class TestBinarySearch:
+    def initialization(self, arr: list):
+        self.arr = BinarySearch(arr)
+
+
+    def test_iterative(self,target):
+        print(f"index of {target} in {self.arr.BinarySearch(target)}")
+
+    def test_recursive(self,target):
+        print(f"index of {target} in {self.arr.BinarySearchRecursive(target)}")
+
+def main():
+    lis = TestBinarySearch()
+    lis.initialization([1,2,3,3,4,6,8,9,10])
+    lis.test_iterative(6)
+    lis.test_recursive(8)
+
+if __name__ == "__main__":
+    main()
