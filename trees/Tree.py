cec04bd433d105c5896b27fce01e968ef58fb7f4
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2024-08-15 10:29:07 -0400

    initialize test and add dfs and bfs

--- trees/Tree.py
+++ trees/Tree.py
@@ -2,8 +2,7 @@ class TreeNode:
     def __init__(self,value):
         self.value = value
         self.children = []
-
-        
+ 
     def add_child(self, child):
         self.children.append(child)
 
@@ -23,3 +22,25 @@ class TreeNode:
             if level != None:
                 return level
         return None
+    
+    def breadth_first_traversal(self):
+        queue = [self]
+        res = []
+        while queue:
+            node = queue.pop(0)
+            res.append(node)
+            queue += node.children
+        return res
+    
+    def depth_first_traversal(self):
+        stack = [self]
+        res = []
+        while stack:
+            node = stack.pop()
+            res.append(node)
+            stack += node.children.reverse()
+        return res
+    
+    #print in form of bfs
+    def __str__(self):
+        return ",".join(self.breadth_first_traversal())
--- trees/main.py
+++ trees/main.py
@@ -1,3 +1,25 @@
-class testTree:
-    def __init__():
-        pass
+from Tree import TreeNode
+class TestTree:
+    def initialization():
+        a = TreeNode("A")
+        b = TreeNode("B")
+        c = TreeNode("C")
+        d = TreeNode("D")
+        e = TreeNode("E")
+        f = TreeNode("F")
+        g = TreeNode("G")
+        h = TreeNode("H")
+
+        a.add_child(b)
+        a.add_child(c)
+        b.add_child(d)
+        b.add_child(e)
+        c.add_child(f)
+        d.add_child(g)
+        d.add_child(h)
+
+        return a
+
+if __name__ == "__main__":
+    a = TestTree.initialization()
+    print(a)
