50faa238ed19dd1741b24d9493702a9e41fe48a1
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2024-09-09 20:56:49 -0400

    implement sift up, print, insert for heap

--- heap/main.py
+++ heap/main.py
@@ -1,11 +1,34 @@
 from MaxHeap import MaxHeap
 class TestHeapMax:
-    def initialization(m: MaxHeap):
-        m.append(5)
-        m.append(6)
-        m.append(9)
-        m.append(10)
-        m.append(22)
-        m.append(15)
-        m.append(26)
-        m.append(4)
+    def initialization(m:MaxHeap):
+        m.insert(5)
+        m.insert(6)
+        m.insert(9)
+        m.insert(10)
+        m.insert(22)
+        m.insert(15)
+        m.insert(26)
+        m.insert(4)
+
+    def test_is_empty(m:MaxHeap):
+        if m.is_empty() == True:
+            print("The heap is empty")
+        print("The heap is not empty")
+
+    def test_get_max(m:MaxHeap):
+        if m.is_empty() == True:
+            print("The heap is empty")
+        else:
+            print(f"The maximum value of the heap is: {m.get_max}")
+
+    def test_print(m:MaxHeap):
+        if m.is_empty() == True:
+            print("The heap is empty")
+        else:
+            print(f"The heap is: {m}")
+
+if __name__ == "__main__":
+    m = MaxHeap()
+    TestHeapMax.initialization(m)
+    TestHeapMax.test_is_empty(m)
+    TestHeapMax.test_print(m)
